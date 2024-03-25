7da15a23e0e58314b19fe066955d04d1014959c1
David wanjohi <davidwagura83@gmail.com>
2024-03-25 11:06:12 +0300

    add a web route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChildController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChildController::class, 'fathersWithChildren']);
